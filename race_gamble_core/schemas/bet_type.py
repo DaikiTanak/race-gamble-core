cd3d0448271f5c758c6c85bc23267b42d34b52e3
DaikiTanak <daiki.yosky@gmail.com>
2024-12-25 00:46:21 +0900

    rm

--- race_gamble_core/schemas/bet_type.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-
-class BetType(StrEnum):
-    tansyou = "tansyou"
-    nirentan = "nirentan"
-    nirenpuku = "nirenpuku"
-    sanrentan = "sanrentan"
-    sanrenpuku = "sanrenpuku"
