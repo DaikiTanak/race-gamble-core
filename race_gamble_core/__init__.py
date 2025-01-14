df6753d73440a5f3696a49bb3800e89cf6df6f2b
DaikiTanak <daiki.yosky@gmail.com>
2025-01-14 14:29:46 +0900

    fix odds class name

--- race_gamble_core/__init__.py
+++ race_gamble_core/__init__.py
@@ -1,10 +1,10 @@
 from .schemas.evaluation_results import BetStrategyResults, EvaluationStatisticResults
-from .schemas.odds import BaseOdds
+from .schemas.odds import Odds
 from .schemas.order import BaseOrder
 
 __all__ = [
     "BetStrategyResults",
     "EvaluationStatisticResults",
-    "BaseOdds",
+    "Odds",
     "BaseOrder",
 ]
